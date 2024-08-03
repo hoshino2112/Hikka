bc47ce21b3628e23d709f29bcbec8e512d394d70
hoshino2112 <hoshino2112@mail.ru>
2024-08-03 23:27:26 +0300

    __init__.py

--- hikka/__init__.py
+++ hikka/__init__.py
@@ -7,9 +7,9 @@
 
 # Do not delete this file, it will cause errors.
 
-__author__ = "Dan Gazizullin"
-__contact__ = "me@hikariatama.ru"
-__copyright__ = "Copyright 2022, Dan Gazizullin"
+__author__ = "hoshino"
+__contact__ = "@hoshino_support_bot"
+__copyright__ = "Copyright 2022, hoshino"
 __credits__ = ["LonamiWebs", "penn5"]
 __license__ = "AGPLv3"
 __maintainer__ = "developer"
