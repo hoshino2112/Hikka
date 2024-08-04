0d9038a361dbbe7c79408ed17715afb72c9e1516
hoshino2112 <hoshino2112@mail.ru>
2024-08-04 17:48:54 +0300

    hikka_info.py

--- hikka/modules/hikka_info.py
+++ hikka/modules/hikka_info.py
@@ -26,7 +26,7 @@ class HikkaInfoMod(loader.Module):
             ),
             loader.ConfigValue(
                 "custom_button",
-                ["🌘 Support chat", "https://t.me/hikka_talks"],
+                ["🌘 Support chat", " , "],
                 lambda: self.strings("_cfg_cst_btn"),
                 validator=loader.validators.Union(
                     loader.validators.Series(fixed_len=2),
@@ -114,7 +114,7 @@ class HikkaInfoMod(loader.Module):
                         (
                             utils.get_platform_emoji()
                             if self._client.hikka_me.premium and not inline
-                            else "🌘 Hikka"
+                            else "hoshino тян"
                         ),
                         "<emoji document_id=5373141891321699086>😎</emoji>",
                         "<emoji document_id=5469741319330996757>💫</emoji>",
